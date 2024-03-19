93c58cba361dd0837519b4fddcaee1e4ebdb5e14
weas3ls <shabaneh.ahmad@gmail.com>
2024-03-19 13:41:55 -0700

    set shop component to accept children

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 
 import Header from "./components/Header.jsx";
+import Product from "./components/Product.jsx";
 import Shop from "./components/Shop.jsx";
+
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
 
 function App() {
@@ -64,7 +66,13 @@ function App() {
     return (
         <>
             <Header cart={shoppingCart} onUpdateCartItemQuantity={handleUpdateCartItemQuantity} />
-            <Shop onAddItemToCart={handleAddItemToCart} />
+            <Shop>
+                {DUMMY_PRODUCTS.map(product => (
+                    <li key={product.id}>
+                        <Product {...product} onAddToCart={handleAddItemToCart} />
+                    </li>
+                ))}
+            </Shop>
         </>
     );
 }
