cef28769dfb0257f04f081e0cc8737e56e3d6b1c
weas3ls <shabaneh.ahmad@gmail.com>
2024-03-19 13:53:57 -0700

    implement cart context to view cart and add items to cart

--- src/App.jsx
+++ src/App.jsx
@@ -65,13 +65,18 @@ function App() {
         });
     }
 
+    const ctxValue = {
+        items: shoppingCart.items,
+        addItemToCart: handleAddItemToCart,
+    };
+
     return (
-        <CartContext.Provider>
+        <CartContext.Provider value={ctxValue}>
             <Header cart={shoppingCart} onUpdateCartItemQuantity={handleUpdateCartItemQuantity} />
             <Shop>
                 {DUMMY_PRODUCTS.map(product => (
                     <li key={product.id}>
-                        <Product {...product} onAddToCart={handleAddItemToCart} />
+                        <Product {...product} />
                     </li>
                 ))}
             </Shop>
